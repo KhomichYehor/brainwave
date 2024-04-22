203970fedc248857ad47d86f9287d9555405df12
Yehor <eee.houmich@gmail.com>
2024-04-22 19:41:53 +0300

    Setup project & add reusable component button & add responcive header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ButtonGradiend from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradiend />
+    </>
+  );
+};
+
+export default App;
